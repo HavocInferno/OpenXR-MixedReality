20f818e6cdc048d0d80fedc660f6a3c79332c3ac
Paul Preissner <preissner.paul@gmail.com>
2020-06-30 16:43:31 +0200

    set Win target platform to 18362 (lower doesn't have lib entries for D3D12Debug)

--- samples/BasicXrApp/OpenXrProgram.h
+++ samples/BasicXrApp/OpenXrProgram.h
@@ -53,7 +53,11 @@ namespace sample {
     public:
         winrt::com_ptr<ID3D12Device> m_pDevice;
         winrt::com_ptr<ID3D12CommandQueue> m_pCommandQueue;
+#ifdef _DEBUG
         bool m_bDebugD3D12 = true;
+#else
+        bool m_bDebugD3D12 = false; 
+#endif // DEBUG
         int m_nMSAASampleCount = 1;
         UINT m_nFrameIndex;
         static const int g_nFrameCount = 2; // Swapchain depth //TODO: exclude companion window items for now, this too?
