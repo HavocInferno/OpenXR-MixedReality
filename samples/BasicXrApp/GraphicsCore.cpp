269ab3c0b204c3912893763cbe7a36aa67a6be31
Paul Preissner <preissner.paul@gmail.com>
2020-07-02 16:25:10 +0200

    hotfix: fix returns and Eigen usage in AddCubeVertex and AddCubeToScene

--- samples/BasicXrApp/GraphicsCore.cpp
+++ samples/BasicXrApp/GraphicsCore.cpp
@@ -467,6 +467,8 @@ bool GraphicsCore::AddCubeVertex(float fl0, float fl1, float fl2, float fl3, flo
     vertdata.push_back(fl2);
     vertdata.push_back(fl3);
     vertdata.push_back(fl4);
+
+    return true;
 }
 
 bool GraphicsCore::AddCubeToScene(Eigen::Matrix4f mat, std::vector<float>& vertdata) {
@@ -482,47 +484,49 @@ bool GraphicsCore::AddCubeToScene(Eigen::Matrix4f mat, std::vector<float>& vertd
     Eigen::Vector4f H = mat * Eigen::Vector4f(0, 1, 1, 1);
 
     // triangles instead of quads
-    AddCubeVertex(E.x, E.y, E.z, 0, 1, vertdata); // Front
-    AddCubeVertex(F.x, F.y, F.z, 1, 1, vertdata);
-    AddCubeVertex(G.x, G.y, G.z, 1, 0, vertdata);
-    AddCubeVertex(G.x, G.y, G.z, 1, 0, vertdata);
-    AddCubeVertex(H.x, H.y, H.z, 0, 0, vertdata);
-    AddCubeVertex(E.x, E.y, E.z, 0, 1, vertdata);
+    AddCubeVertex(E.x(), E.y(), E.z(), 0, 1, vertdata); // Front
+    AddCubeVertex(F.x(), F.y(), F.z(), 1, 1, vertdata);
+    AddCubeVertex(G.x(), G.y(), G.z(), 1, 0, vertdata);
+    AddCubeVertex(G.x(), G.y(), G.z(), 1, 0, vertdata);
+    AddCubeVertex(H.x(), H.y(), H.z(), 0, 0, vertdata);
+    AddCubeVertex(E.x(), E.y(), E.z(), 0, 1, vertdata);
 
-    AddCubeVertex(B.x, B.y, B.z, 0, 1, vertdata); // Back
-    AddCubeVertex(A.x, A.y, A.z, 1, 1, vertdata);
-    AddCubeVertex(D.x, D.y, D.z, 1, 0, vertdata);
-    AddCubeVertex(D.x, D.y, D.z, 1, 0, vertdata);
-    AddCubeVertex(C.x, C.y, C.z, 0, 0, vertdata);
-    AddCubeVertex(B.x, B.y, B.z, 0, 1, vertdata);
+    AddCubeVertex(B.x(), B.y(), B.z(), 0, 1, vertdata); // Back
+    AddCubeVertex(A.x(), A.y(), A.z(), 1, 1, vertdata);
+    AddCubeVertex(D.x(), D.y(), D.z(), 1, 0, vertdata);
+    AddCubeVertex(D.x(), D.y(), D.z(), 1, 0, vertdata);
+    AddCubeVertex(C.x(), C.y(), C.z(), 0, 0, vertdata);
+    AddCubeVertex(B.x(), B.y(), B.z(), 0, 1, vertdata);
 
-    AddCubeVertex(H.x, H.y, H.z, 0, 1, vertdata); // Top
-    AddCubeVertex(G.x, G.y, G.z, 1, 1, vertdata);
-    AddCubeVertex(C.x, C.y, C.z, 1, 0, vertdata);
-    AddCubeVertex(C.x, C.y, C.z, 1, 0, vertdata);
-    AddCubeVertex(D.x, D.y, D.z, 0, 0, vertdata);
-    AddCubeVertex(H.x, H.y, H.z, 0, 1, vertdata);
+    AddCubeVertex(H.x(), H.y(), H.z(), 0, 1, vertdata); // Top
+    AddCubeVertex(G.x(), G.y(), G.z(), 1, 1, vertdata);
+    AddCubeVertex(C.x(), C.y(), C.z(), 1, 0, vertdata);
+    AddCubeVertex(C.x(), C.y(), C.z(), 1, 0, vertdata);
+    AddCubeVertex(D.x(), D.y(), D.z(), 0, 0, vertdata);
+    AddCubeVertex(H.x(), H.y(), H.z(), 0, 1, vertdata);
 
-    AddCubeVertex(A.x, A.y, A.z, 0, 1, vertdata); // Bottom
-    AddCubeVertex(B.x, B.y, B.z, 1, 1, vertdata);
-    AddCubeVertex(F.x, F.y, F.z, 1, 0, vertdata);
-    AddCubeVertex(F.x, F.y, F.z, 1, 0, vertdata);
-    AddCubeVertex(E.x, E.y, E.z, 0, 0, vertdata);
-    AddCubeVertex(A.x, A.y, A.z, 0, 1, vertdata);
+    AddCubeVertex(A.x(), A.y(), A.z(), 0, 1, vertdata); // Bottom
+    AddCubeVertex(B.x(), B.y(), B.z(), 1, 1, vertdata);
+    AddCubeVertex(F.x(), F.y(), F.z(), 1, 0, vertdata);
+    AddCubeVertex(F.x(), F.y(), F.z(), 1, 0, vertdata);
+    AddCubeVertex(E.x(), E.y(), E.z(), 0, 0, vertdata);
+    AddCubeVertex(A.x(), A.y(), A.z(), 0, 1, vertdata);
 
-    AddCubeVertex(A.x, A.y, A.z, 0, 1, vertdata); // Left
-    AddCubeVertex(E.x, E.y, E.z, 1, 1, vertdata);
-    AddCubeVertex(H.x, H.y, H.z, 1, 0, vertdata);
-    AddCubeVertex(H.x, H.y, H.z, 1, 0, vertdata);
-    AddCubeVertex(D.x, D.y, D.z, 0, 0, vertdata);
-    AddCubeVertex(A.x, A.y, A.z, 0, 1, vertdata);
+    AddCubeVertex(A.x(), A.y(), A.z(), 0, 1, vertdata); // Left
+    AddCubeVertex(E.x(), E.y(), E.z(), 1, 1, vertdata);
+    AddCubeVertex(H.x(), H.y(), H.z(), 1, 0, vertdata);
+    AddCubeVertex(H.x(), H.y(), H.z(), 1, 0, vertdata);
+    AddCubeVertex(D.x(), D.y(), D.z(), 0, 0, vertdata);
+    AddCubeVertex(A.x(), A.y(), A.z(), 0, 1, vertdata);
 
-    AddCubeVertex(F.x, F.y, F.z, 0, 1, vertdata); // Right
-    AddCubeVertex(B.x, B.y, B.z, 1, 1, vertdata);
-    AddCubeVertex(C.x, C.y, C.z, 1, 0, vertdata);
-    AddCubeVertex(C.x, C.y, C.z, 1, 0, vertdata);
-    AddCubeVertex(G.x, G.y, G.z, 0, 0, vertdata);
-    AddCubeVertex(F.x, F.y, F.z, 0, 1, vertdata);
+    AddCubeVertex(F.x(), F.y(), F.z(), 0, 1, vertdata); // Right
+    AddCubeVertex(B.x(), B.y(), B.z(), 1, 1, vertdata);
+    AddCubeVertex(C.x(), C.y(), C.z(), 1, 0, vertdata);
+    AddCubeVertex(C.x(), C.y(), C.z(), 1, 0, vertdata);
+    AddCubeVertex(G.x(), G.y(), G.z(), 0, 0, vertdata);
+    AddCubeVertex(F.x(), F.y(), F.z(), 0, 1, vertdata);
+
+    return true; 
 }
 
 const std::vector<DXGI_FORMAT>& GraphicsCore::SupportedColorFormats() const {
