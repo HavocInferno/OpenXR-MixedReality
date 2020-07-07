651e9460abcbdb4e9f25526b0d5d7fda5929df02
Paul Preißner <paul.preissner@echtzeitgraphik.de>
2020-07-07 16:44:52 +0200

    WIP adapting HelloVR scene.hlsl shader
    to BasicXrApp VPRT single pass stereo and separate model/VP matrices

--- samples/BasicXrApp/shaders/scene.hlsl
+++ samples/BasicXrApp/shaders/scene.hlsl
@@ -4,17 +4,22 @@ struct VS_INPUT
 {
 	float3 vPosition : POSITION;
 	float2 vUVCoords: TEXCOORD0;
+    uint instId : SV_InstanceID;
 };
 
 struct PS_INPUT
 {
 	float4 vPosition : SV_POSITION;
 	float2 vUVCoords : TEXCOORD0;
+    uint viewId : SV_RenderTargetArrayIndex;
 };
 
 cbuffer SceneConstantBuffer : register(b0)
 {
 	float4x4 g_MVPMatrix;
+};
+cbuffer ViewProjectionConstantBuffer : register(b1) {
+    float4x4 ViewProjection[2];
 };
 
 SamplerState g_SamplerState : register(s0);
@@ -24,11 +29,13 @@ Texture2D g_Texture : register(t0);
 PS_INPUT VSMain( VS_INPUT i )
 {
 	PS_INPUT o;
-	o.vPosition = mul( g_MVPMatrix, float4( i.vPosition, 1.0 ) );
+	//o.vPosition = mul( g_MVPMatrix, float4( i.vPosition, 1.0 ) );
+	o.vPosition = mul(mul(float4(i.vPosition, 1), g_MVPMatrix), ViewProjection[i.instId]);
 #ifdef VULKAN
 	o.vPosition.y = -o.vPosition.y;
 #endif
 	o.vUVCoords = i.vUVCoords;
+    o.viewId = i.instId; 
 	return o;
 }
 
